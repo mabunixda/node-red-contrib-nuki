966f9f9adf2a13e039f1ca4deee4240245443120
Martin Buchleitner <mabunixda@gmail.com>
2019-05-09 10:41:26 +0200

    initial commit of nuki node package
    Signed-off-by: Martin Buchleitner <mabunixda@gmail.com>

--- README.md
+++ README.md
@@ -1,2 +1,84 @@
 # node-red-contrib-nuki
-node for nuki
+
+This node can control Nuki Smart Locks with the help of Nuki Bridges. 
+Also Nuki Bridges can be handled by the node. It uses [nuki-bridge-api](https://github.com/Mik13/nuki-bridge-api/) based on the Nuki Bridge API documentation.
+
+It enables you to connecto to your Nuki Smart Lock over a Nuki Bridge and control it directly from node-red. 
+It also enables you to monitor the states and control the bridge itself.
+
+## Nodes
+
+### Bridge
+
+Configure a Nuki Bridge connection used by other nodes
+
+### Control In
+
+Send commands and queries to a Nuki Smart Lock. At the moment 2 actions are available which can be set as **topic**
+
+##### lockStatus
+
+query the lockStatus of the current lock. Possible states can be reviewd at [state of nuki-bridge-api](https://github.com/Mik13/nuki-bridge-api/blob/master/lib/lock-state.js#L6). Those are at the moment
+```
+UNCALIBRATED
+LOCKED
+UNLOCKING
+UNLOCKED
+LOCKING
+UNLATCHED
+UNLOCKED_LOCK_N_GO
+UNLATCHING
+MOTOR_BLOCKED
+UNDEFINED
+```
+
+#### lockAction
+
+Performs an action on the current lock. Actions are not validated within the node if they are valid - e.g. the lock is already unlocked and you send an unlock again. In this case the bridge itself manages this and does not try to unlock again. Available actions are directly mapped to the [actions of nuki-bridge-api](https://github.com/Mik13/nuki-bridge-api/blob/master/lib/lock-action.js). At the moment those actions are:
+```
+UNLOCK
+LOCK
+UNLATCH
+LOCK_N_GO
+LOCK_N_GO_WITH_UNLATCH
+```
+
+### Bridge Control
+
+Send commands and queries to a Nuki Bridge.
+
+The nuki bridge supports various commands. At the moment the responses are directly forwarded as output payload and are not parsed. They are only enriched with the original topic and bridge information ( host and port ).
+
+#### reboot
+
+Of course this action reboots the bridge itself
+
+#### fwupdate
+
+Update the firmware of the nuki bridge. It is not possible right now if there is an update available!
+
+#### info
+
+Retrieve information about the bridge. The payload looks like following example
+```json
+{"topic":"info","bridge":"192.168.1.100:8080","payload":{"bridgeType":1,"ids":{"hardwareId":234189529,"serverId":291317518},"versions":{"firmwareVersion":"1.12.6","wifiFirmwareVersion":"1.2.0"},"uptime":75492,"currentTime":"2019-05-09T08:09:20+00:00","serverConnected":true,"scanResults":[]},"_msgid":"e3cab797.caec98"}
+```
+
+#### log
+
+Retrieve the current log from the bridge. Right now only the last 100 lines are reported even the api is available to page through the available logs
+
+#### clearlog
+
+Clear all logs on the bridge
+
+## Todo
+
+* Support of callack provided by nuki-bridge-api to get events of Nuki Smart Lock actions
+* Paging of Nuki Bridge logs
+* Nuki Smart Lock Door sensor support ( missing in nuki-bridge-api )
+* More testing in the wild
+
+
+## Caveats
+At the moment the door sensor is not support within nuki-bridge-api. I hope to get into this topic by the next time.
---
+++ nuki/nuki.js
@@ -0,0 +1,232 @@
+module.exports = function (RED) {
+
+  "use strict";
+  const bridgeAPI = require('nuki-bridge-api');
+  const lockStates = bridgeAPI.lockState;
+  const lockActions = bridgeAPI.lockAction;
+
+  function getLockState(lockState) {
+    for (var x in lockStates) {
+      if (lockStates[x] === lockState)
+        return x;
+    }
+    return undefined;
+  }
+
+
+  RED.httpAdmin.get('/nuki-bridge/list', function (req, res) {
+    if (!req.query.id) {
+      return res.json("");
+    }
+
+    var configNode = RED.nodes.getNode(req.query.id);
+    var result = {
+      state: 'error',
+      msg: 'bridge not connected',
+      items: []
+    };
+
+    if (configNode && configNode.bridge && configNode.nukis) {
+
+      var data = [];
+      for (var i = 0; i < configNode.nukis.length; ++i) {
+        var n = configNode.nukis[i];
+        var nuki = {};
+        nuki.id = n.nukiId;
+        nuki.name = n.name;
+
+        data.push(nuki);
+      }
+      console.log(data);
+      result = {
+        state: 'ok',
+        msg: 'got nuki list',
+        items: data
+      };
+    }
+    res.json(result)
+  });
+
+
+  function NukiBridge(config) {
+    RED.nodes.createNode(this, config);
+
+    var node = this;
+    node.host = config.host;
+    node.port = config.port;
+    node._bridgeNodes = [];
+    node._nukiNodes = [];
+    node.nukis = [];
+
+    node.bridge = bridgeAPI.Bridge(node.host, node.port, node.credentials.token);
+    node.bridge.list().then(function getNuki(nukis) {
+      node.nukis = nukis;
+      console.log("Got " + node.nukis.length + " nukis from bridge " + node.host);
+    });
+  }
+
+  NukiBridge.prototype.registerNukiNode = function (handler) {
+    this._nukiNodes.push(handler);
+  }
+  NukiBridge.prototype.deregisterNukiNode = function (handler) {
+    this._nukiNodes.forEach(function (node, i, nukiNodes) {
+      if (node === handler) {
+        nukiNodes.splice(i, 1);
+      }
+    });
+  }
+
+  NukiBridge.prototype.registerBridgeNode = function (handler) {
+    this._bridgeNodes.push(handler);
+  }
+  NukiBridge.prototype.deregisterBridgeNode = function (handler) {
+    this._bridgeNodes.forEach(function (node, i, bridgeNodes) {
+      if (node === handler) {
+        bridgeNodes.splice(i, 1);
+      }
+    });
+  }
+  NukiBridge.prototype.handleBridgeEvent = function (uuid, event) {
+    var payload;
+    try {
+      payload = JSON.parse(event);
+    } catch (err) {
+      payload = event;
+    }
+    for (var i = 0; i < this._bridgeNodes.length; i++) {
+      if (this._bridgeNodes[i].id !== uuid) {
+        continue;
+      }
+      var currentNode = this._bridgeNodes[i];
+      var msg = {
+        topic: payload.topic,
+        bridge: currentNode.name
+      }
+
+      if (payload.topic.toLowerCase() === 'reboot') {
+        this.bridge.reboot().then(function (response) {
+          msg.payload = response;
+          currentNode.send(msg);
+        });
+      } else if (payload.topic.toLowerCase() === 'fwupdate') {
+        this.bridge.fwupdate().then(function (response) {
+          msg.payload = response;
+          currentNode.send(msg);
+        })
+      } else if (payload.topic.toLowerCase() === 'info') {
+        this.bridge.info().then(function (response) {
+          msg.payload = response;
+          currentNode.send(msg);
+        });
+      } else if (payload.topic.toLowerCase() === 'log') {
+        var offset = undefined;
+        var count = undefined;
+        this.bridge.log(offset, count).then(function (logLines) {
+          msg.payload = logLines;
+          currentNode.send(msg);
+        })
+      } else if (payload.topic.toLowerCase() === 'clearlog') {
+        this.bridge.clearlog().then(function (response) {
+          msg.payload = response;
+          currentNode.send(msg);
+        })
+      }
+      return;
+    }
+  }
+  NukiBridge.prototype.handleEvent = function (uuid, event) {
+    var payload;
+    try {
+      payload = JSON.parse(event);
+    } catch (err) {
+      payload = event;
+    }
+    console.log(payload);
+    var msg = {
+      topic: payload.topic,
+    }
+
+    for (var i = 0; i < this._nukiNodes.length; i++) {
+      if (this._nukiNodes[i].id !== uuid) {
+        continue;
+      }
+      var underControl = this._nukiNodes[i];
+      for (var x = 0; x < this.nukis.length; ++x) {
+        if (this.nukis[x].nukiId != underControl.nuki) {
+          continue;
+        }
+
+        var currentNuki = this.nukis[x].nuki;
+        msg.nuki = this.nukis[x].name;
+        msg.nukiId = this.nukis[x].nukiId;
+
+        if (payload.topic.toLowerCase() === 'lockaction') {
+          var action = lockActions[payload.payload];
+          currentNuki.lockAction(action).then(function (status) {
+            msg.payload = status;
+            underControl.send(msg);
+          });
+        } else if (payload.topic.toLowerCase() === "lockstatus") {
+          currentNuki.lockState().then(function (lockState) {
+            var state = getLockState(lockState);
+            msg.payload = {
+              state: state,
+              value: lockState
+            };
+            underControl.send(msg);
+          });
+        }
+        return;
+      }
+    }
+  }
+
+  RED.nodes.registerType("nuki-bridge", NukiBridge, {
+    credentials: {
+      token: {
+        type: "password"
+      }
+    }
+  });
+
+  function NukiLockControl(config) {
+    RED.nodes.createNode(this, config);
+    var node = this;
+    node.bridge = RED.nodes.getNode(config.bridge);
+    node.nuki = config.nuki;
+    if (node.bridge) {
+      node.bridge.registerNukiNode(node);
+      this.on('close', function (done) {
+        if (node.bridge) {
+          node.bridge.deregisterNukiNode(node);
+        }
+        done();
+      });
+      this.on('input', function (msg) {
+        node.bridge.handleEvent(node.id, msg);
+      });
+    }
+  }
+  RED.nodes.registerType("nuki-lock-control", NukiLockControl);
+
+  function NukiBridgeControl(config) {
+    RED.nodes.createNode(this, config);
+    var node = this;
+    node.bridge = RED.nodes.getNode(config.bridge);
+    if (node.bridge) {
+      node.bridge.registerBridgeNode(node);
+      this.on('close', function (done) {
+        if (node.bridge) {
+          node.bridge.deregisterBridgeNode(node);
+        }
+        done();
+      });
+      this.on('input', function (msg) {
+        node.bridge.handleBridgeEvent(node.id, msg);
+      });
+    }
+  }
+  RED.nodes.registerType("nuki-bridge-control", NukiBridgeControl);
+
+
+}
