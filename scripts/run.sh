4a0e8225c8771172505286a0b8086ac2cf40b0fd
Martin Buchleitner <mabunixda@gmail.com>
2021-09-28 13:12:34 +0000

    fix #30

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-podman run -u root --rm -it --network host -v "$PWD:/src" --entrypoint /src/scripts/docker-run.sh docker.io/nodered/node-red:latest-14-minimal
+cmd="podman"
+has_podman=$(hash docker 2> /dev/null)
+if [ "$?" == "0" ]; then
+    cmd="docker"
+fi
+
+$cmd run \
+    -u root \
+    --rm -it \
+    --network host \
+    --entrypoint /src/scripts/docker-run.sh \
+    -v "$PWD/:/src" \
+    docker.io/nodered/node-red:latest-minimal
